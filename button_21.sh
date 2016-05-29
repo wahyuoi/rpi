1bd88a80c06f569f65fe82902b7f605392ff7b6b
Gede Wahyu <gedewahyu.ap@gmail.com>
2016-05-29 14:48:46 +0000

    set button to power off rpi

--- button_21.sh
+++ button_21.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 gpio -g mode 21 input;
-while [ gpio -g read 21 == 0]; 
+gpio -g mode 19 output;
+gpio -g mode 26 output;
+gpio -g write 19 1;
+gpio -g write 26 0;
+while [ true ];
 do
+ val=`gpio -g read 21`;
+ if [ $val -eq 1 ]; then
+  gpio -g write 19 0;
+  echo "Power Off";
+  echo "Good Bye!";
+  val=`sudo poweroff`;
+ fi;
+ sleep .41;
 done
 echo "------";
